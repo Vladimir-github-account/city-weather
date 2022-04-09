b744dfe5d2a2ec761e9cfc66b6854abca55821fd
vladimir-github-account <volodymyr.dashkevychit@gmail.com>
2022-04-09 17:14:43 +0300

    Add: loader spinner Refactor

--- src/components/WeatherLoading.tsx
+++ src/components/WeatherLoading.tsx
@@ -3,6 +3,7 @@ import {useTypedSelector} from "../hooks/useTypedSelector";
 import {fetchWeather} from "../store/action-creators/weather";
 import {useActions} from "../hooks/useActions";
 import WeatherItem from "./WeatherItem";
+import { Bars } from  'react-loader-spinner'
 
 const WeatherLoading: FC = () => {
     const {weather, loading, error} = useTypedSelector(state => state.weather)
@@ -11,7 +12,7 @@ const WeatherLoading: FC = () => {
         fetchWeather()
     }, [])
     if (loading) {
-        return <h1>Loading...</h1>
+        return	<Bars color="#00BFFF" height={80} width={80} ariaLabel='loading' />
     }
     if (error) {
         return <h1>{error}</h1>
--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,8 +1,7 @@
 import {useDispatch} from "react-redux";
 import {bindActionCreators} from "redux";
-import * as WeatherActionCreators from '../store/action-creators/weather'
-
+import ActionCreators from '../store/action-creators/'
 export const useActions = () => {
     const dispatch = useDispatch()
-    return bindActionCreators(WeatherActionCreators, dispatch);
+    return bindActionCreators(ActionCreators, dispatch);
 }
