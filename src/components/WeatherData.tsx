c1dbab43bfa0ca924708d4cb0bcb4dc2e84a65c1
volodymyr <volodymyr.dashkevychit@gmail.com>
2023-06-28 21:19:11 +0300

    nav links fix

--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -1,24 +1,30 @@
-import React, { FC, useState } from 'react';
-import WeatherDetails          from './WeatherDetails';
-import { IWeatherResponse }    from '../types/weather';
-import WeatherHourlyChart      from './WeatherHourlyChart';
-import { Route, Routes }       from 'react-router-dom';
-import { Box }                 from '@mui/material';
-import NavButton               from './UI/NavButton';
-import { navLinkItems }        from '../constants';
-import WeatherDailyChart       from './WeatherDailyChart';
+import React, { FC, useState }     from 'react';
+import WeatherDetails              from './WeatherDetails';
+import { IWeatherResponse }        from '../types/weather';
+import WeatherHourlyChart          from './WeatherHourlyChart';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Box }                     from '@mui/material';
+import NavButton                   from './UI/NavButton';
+import { navLinkItems }            from '../constants';
+import WeatherDailyChart           from './WeatherDailyChart';
 
 interface WeatherDataProps {
 	weather: IWeatherResponse;
 }
 
+const getNextNavLinkItem = (navLink: string) => {
+	if (navLinkItems.includes(navLink)) {
+		const currentIndex = navLinkItems.indexOf(navLink);
+		return navLinkItems[(currentIndex + 1) % navLinkItems.length];
+	}
+	return navLinkItems[1];
+};
+
 const WeatherData: FC<WeatherDataProps> = ({ weather }) => {
 	const isWeatherLoaded = Object.keys(weather).length > 0;
-	const [navLink, setNavLink] = useState(navLinkItems[1]);
+	const [navLink, setNavLink] = useState(getNextNavLinkItem(document.location.pathname));
 	const handleClick = () => {
-		const currentIndex = navLinkItems.indexOf(navLink);
-		const nextIndex = (currentIndex + 1) % navLinkItems.length;
-		setNavLink(navLinkItems[nextIndex]);
+		setNavLink(getNextNavLinkItem(navLink));
 	};
 	return (
 		<Box className="h-1/2 min-h-390px flex flex-col justify-between mt-9 md:mt-6 2xl:mt-10 overflow-x-auto overflow-y-hidden">
@@ -27,6 +33,7 @@ const WeatherData: FC<WeatherDataProps> = ({ weather }) => {
 					<Route path="/" element={(<WeatherDetails weatherResponse={weather}/>)}/>
 					<Route path="/hourly" element={(<WeatherHourlyChart weatherResponse={weather}/>)}/>
 					<Route path="/daily" element={(<WeatherDailyChart weatherResponse={weather}/>)}/>
+					<Route path="/*" element={(<Navigate to="/"/>)}/>
 				</Routes>}
 			<hr className={`border-b-1 opacity-40 ${!isWeatherLoaded && 'mt-auto'}`}/>
 			{isWeatherLoaded && <NavButton link={navLink} handleClick={handleClick}/>}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,1 @@
-export const navLinkItems = ['', '/hourly', '/daily'];
+export const navLinkItems = ['/', '/hourly', '/daily'];
